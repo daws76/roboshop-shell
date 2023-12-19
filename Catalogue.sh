9c947fe2082a049a4186955451db2cf57af238cf
daws76 <schandradba9@gmail.com>
2023-12-19 06:51:24 +0530

    mongodb

--- Catalogue.sh
+++ Catalogue.sh
@@ -5,7 +5,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-MONGODB_HOST=mongodb.daws76.site
+MONGODB_HOST="mongodb.daws76.site"
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
