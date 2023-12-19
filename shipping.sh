41c817505a99fc251da6d6799a59004c82ce828c
daws76 <schandradba9@gmail.com>
2023-12-19 12:27:54 +0530

    mongodb

--- shipping.sh
+++ shipping.sh
@@ -47,29 +47,53 @@ VALIDATE $? "Creating  app directory"
 
 curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip &>> $LOGFILE 
 
+VALIDATE $? "Downloading shipping"
+
 cd /app
 
+VALIDATE $? "moving to app directory"
+
 unzip -o /tmp/shipping.zip &>> $LOGFILE 
+
+VALIDATE $? "Unzipping shipping"
 
 cd /app
 
 mvn clean package &>> $LOGFILE 
 
+VALIDATE $? "Installing dependencies"
+
 mv target/shipping-1.0.jar shipping.jar &>> $LOGFILE 
+
+VALIDATE $? "renaming jar files"
 
 cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service &>> $LOGFILE 
 
+VALIDATE $? "Coping shipping service"
+
 systemctl daemon-reload &>> $LOGFILE 
+
+VALIDATE $? "Deemon reload"
 
 systemctl enable shipping &>> $LOGFILE 
 
+VALIDATE $? "Enabling shipping"
+
 systemctl start shipping &>> $LOGFILE 
+
+VALIDATE $? "Starting shipping"
 
 dnf install mysql -y &>> $LOGFILE 
 
+VALIDATE $? "Installing mysql client"
+
 mysql -h mysql.daws76.site -uroot -pRoboShop@1 < /app/schema/shipping.sql &>> $LOGFILE 
 
+VALIDATE $? "Loading the shipping data"
+
 systemctl restart shipping &>> $LOGFILE 
+
+VALIDATE $? "restarting shipping"
 
 
 
