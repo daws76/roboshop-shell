bbfd10efe3c75406a0bdbcf2f49fcac2107a6c83
daws76 <schandradba9@gmail.com>
2023-12-19 10:09:15 +0530

    mongodb

---
+++ user.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MONGODB_HOST="mongodb.daws76.site"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE 
+
+VALIDATE(){
+  if [ $1 -ne 0 ]
+  then 
+    echo -e "$2 .. $R Failed $N"
+    exit 1
+  else 
+    echo -e "$2 .. $G Success $N"  
+   fi    
+} 
+
+if [ $ID -ne 0 ]
+then 
+  echo -e "$R Error please run with root user $N"
+  exit 1
+else
+  echo "you are root user"
+fi
+
+dnf module disable nodejs -y &>> $LOGFILE
+
+VALIDATE $? "Disabling current Nodejs"
+
+dnf module enable nodejs:18 -y &>> $LOGFILE
+
+VALIDATE $? "Enabling current Nodejs"
+
+dnf install nodejs -y &>> $LOGFILE
+
+VALIDATE $? "Installing  current version Nodejs18"
+
+id roboshop
+if [ $? -ne 0 ]
+then 
+   useradd roboshop &>> $LOGFILE
+   VALIDATE $? "roboshop user creation"
+else 
+   echo -e "roboshop user is already exist $Y Skipping $N"  
+fi 
+
+mkdir -p /app &>> $LOGFILE
+
+VALIDATE $? "Creating  app directory"
+
+curl -o /tmp/user.zip https://roboshop-builds.s3.amazonaws.com/user.zip &>> $LOGFILE
+
+VALIDATE $? "Downloading catalouge applicaion"
+
+cd /app &>> $LOGFILE
+
+unzip -o /tmp/user.zip &>> $LOGFILE
+
+VALIDATE $? "Unziping user"
+
+npm install &>> $LOGFILE
+
+VALIDATE $? "Installing dependencies"
+
+cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service
+
+VALIDATE $? "Copying user service file"
+
+systemctl daemon-reload &>> $LOGFILE
+
+VALIDATE $? "user daemon reload"
+
+systemctl enable user &>> $LOGFILE
+
+VALIDATE $? "Enabling user"
+
+systemctl start user &>> $LOGFILE
+
+VALIDATE $? "Starting user"
+
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+
+VALIDATE $? "Copying mongorepo"
+
+dnf install mongodb-org-shell -y &>> $LOGFILE
+
+VALIDATE $? "Installing mongodb client"
+
+mongo --host $MONGODB_HOST </app/schema/user.js &>> $LOGFILE
+
+VALIDATE $? "Loading user data to mongodb"
+
