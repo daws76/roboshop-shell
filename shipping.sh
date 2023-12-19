1a886b8b0d69c17f104d0ac6b03d921d64a1be9a
daws76 <schandradba9@gmail.com>
2023-12-19 12:19:46 +0530

    mongodb

--- shipping.sh
+++ shipping.sh
@@ -30,3 +30,46 @@ else
   echo "you are root user"
 fi
 
+dnf install maven -y &>> $LOGFILE 
+
+id roboshop
+if [ $? -ne 0 ]
+then 
+   useradd roboshop &>> $LOGFILE
+   VALIDATE $? "roboshop user creation"
+else 
+   echo -e "roboshop user is already exist $Y Skipping $N"  
+fi 
+
+mkdir -p /app &>> $LOGFILE 
+
+VALIDATE $? "Creating  app directory"
+
+curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip &>> $LOGFILE 
+
+cd /app
+
+unzip -o /tmp/shipping.zip &>> $LOGFILE 
+
+cd /app
+
+mvn clean package &>> $LOGFILE 
+
+mv target/shipping-1.0.jar shipping.jar &>> $LOGFILE 
+
+cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service &>> $LOGFILE 
+
+systemctl daemon-reload &>> $LOGFILE 
+
+systemctl enable shipping &>> $LOGFILE 
+
+systemctl start shipping &>> $LOGFILE 
+
+dnf install mysql -y &>> $LOGFILE 
+
+mysql -h mysql.daws76.site -uroot -pRoboShop@1 < /app/schema/shipping.sql &>> $LOGFILE 
+
+systemctl restart shipping &>> $LOGFILE 
+
+
+
